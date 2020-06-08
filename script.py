079786a9266df4159e58e15588ce829e882e9fb6
Sachin Iyer <sachinjiyer@gmail.com>
2020-06-08 09:32:18 -0700

    modified:   script.py

--- script.py
+++ script.py
@@ -13,31 +13,46 @@ if len(sys.argv) != 3:
 Username = sys.argv[1]
 Password = sys.argv[2]
 
-driver = webdriver.Chrome()
+# Start headless Chrome without display
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--remote-debugging-port=9222")
+options.headless = True
+command_executor = "http://localhost:4444/wd/hub"
+driver = webdriver.Remote(command_executor, desired_capabilities=options.to_capabilities())
+
+# driver = webdriver.Chrome(executable_path = "/Users/sachiniyer/docs/coding/Python/env/projects/web_automation/chromedriver")
 driver.get('https://sameday.costco.com/')
 
+# Enter zip code
 zip_code = driver.find_element_by_id('signup-zipcode')
 zip_code.send_keys('95125')
 
+# Submit button
 continue_box = driver.find_element_by_class_name('rcp-form-submit-button')
 driver.implicitly_wait(2)
 continue_box.click()
 
+# Enter email
 driver.implicitly_wait(2)
 email_box = driver.find_element_by_id('logonId')
 email_box.send_keys(Username)
 
+# Enter password
 pass_box = driver.find_element_by_id('logonPassword')
 pass_box.send_keys(Password)
 
+# Continue box
 continue_box = driver.find_elements(By.CLASS_NAME, "primary")
 driver.implicitly_wait(1)
 continue_box[1].click()
 
+# Open deliveries
 driver.implicitly_wait(5)
 button = driver.find_element_by_xpath("//*[@id='header']/div/div/div[5]/div[2]/span/a")
 button.click()
 
+# Open deliveries 2
 driver.implicitly_wait(5)
 button = driver.find_element_by_xpath("//*[@id='react-tabs-2']")
 button.click()
